33512b5cfed52d8e1af4a6698d07a1d90bfe7d4e
William Tran <wtran29@gmail.com>
2019-08-19 15:08:56 -0700

    created form fields and labels for streamCreate, included validation

--- src/components/streams/StreamCreate.js
+++ src/components/streams/StreamCreate.js
@@ -1,7 +1,46 @@
 import React from 'react';
+import { Field, reduxForm } from 'redux-form';
 
-const StreamCreate = () => {
-    return <div>StreamCreate</div>;
+class StreamCreate extends React.Component {
+    renderInput({ input, label, meta }) {
+        return (
+            <div className="field">
+                <label>{label}</label>
+                <input {...input} />
+                <div>{meta.error}</div>
+            </div>
+        );
+    }
+
+    onSubmit(formValues) {
+        console.log(formValues);        
+    }
+
+    render() {
+        return (
+            <form onSubmit={this.props.handleSubmit(this.onSubmit)} className="ui form">
+                <Field name="title" component={this.renderInput} label="Enter Title" />
+                <Field
+                    name="description"
+                    component={this.renderInput}
+                    label="Enter Description"
+                />
+                <button className="ui button primary">Submit</button>
+            </form>
+        )
+    };
 };
 
-export default StreamCreate;
+const validate = (formValues) => {
+    const errors = {};
+    if (!formValues.title) {
+        errors.title = 'Must enter a title';
+    }
+    if (!formValues.description) {
+        errors.description = 'Must enter description';
+    }
+
+    return errors;
+};
+
+export default reduxForm({ form: 'streamCreate', validate })(StreamCreate);
