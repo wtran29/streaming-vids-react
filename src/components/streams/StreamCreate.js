f5db5b1659cc3cb92c1cb9704c9ba0f439e54fd7
William Tran <wtran29@gmail.com>
2019-08-19 17:23:16 -0700

    created json-server for handling backend, created functionality for streamCreate component, set up actions/reducers for rest of streams for edit, delete, show and list, include redux-thunk middleware

--- src/components/streams/StreamCreate.js
+++ src/components/streams/StreamCreate.js
@@ -1,24 +1,36 @@
 import React from 'react';
 import { Field, reduxForm } from 'redux-form';
+import { connect } from 'react-redux';
+import { createStream } from '../../actions';
 
 class StreamCreate extends React.Component {
-    renderInput({ input, label, meta }) {
+    renderError({ error, touched }) {
+        if (touched && error) {
+            return (
+                <div className="ui error">
+                    <div className="header">{error}</div>
+                </div>
+            )
+        }
+    }
+    renderInput = ({ input, label, meta }) => {
+        const className=`field ${meta.error && meta.touched ? 'error': ''}`
         return (
-            <div className="field">
+            <div className={className}>
                 <label>{label}</label>
-                <input {...input} />
-                <div>{meta.error}</div>
+                <input {...input} autoComplete="off" />
+                {this.renderError(meta)}
             </div>
         );
     }
 
-    onSubmit(formValues) {
-        console.log(formValues);        
+    onSubmit = (formValues) => {
+        this.props.createStream(formValues);       
     }
 
     render() {
         return (
-            <form onSubmit={this.props.handleSubmit(this.onSubmit)} className="ui form">
+            <form onSubmit={this.props.handleSubmit(this.onSubmit)} className="ui form error">
                 <Field name="title" component={this.renderInput} label="Enter Title" />
                 <Field
                     name="description"
@@ -43,4 +55,6 @@ const validate = (formValues) => {
     return errors;
 };
 
-export default reduxForm({ form: 'streamCreate', validate })(StreamCreate);
+const formWrapped = reduxForm({ form: 'streamCreate', validate })(StreamCreate);
+
+export default connect(null, { createStream })(formWrapped);
